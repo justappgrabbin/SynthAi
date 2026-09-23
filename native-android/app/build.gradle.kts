1e522bb8e220f2ac3fd86c7dfd9b630e884f02e0
justappgrabbin <bookingalexismcmillion@gmail.com>
2026-09-23 09:59:51 -0700

    Add native Android seed app build

---
+++ native-android/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "org.synthai.computer"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "org.synthai.computer"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0-native-seed"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
