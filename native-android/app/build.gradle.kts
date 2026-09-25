5bf387d0cbbc8a7bdce66ac95b88a40eba8f9177
justappgrabbin <bookingalexismcmillion@gmail.com>
2026-09-25 01:00:10 -0700

    Android: store Linux residence archives without recompression

--- native-android/app/build.gradle.kts
+++ native-android/app/build.gradle.kts
@@ -24,6 +24,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    androidResources {
+        noCompress += listOf("gz", "synthimg")
+    }
 }
 
 
